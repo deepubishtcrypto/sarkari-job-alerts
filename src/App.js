866867cf770d996849445bcf0b7390488a9d2d29
deepubishtcrypto <ib811639@gmail.com>
2025-06-11 12:57:08 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+function App() {
+  return (
+    <div className="p-4 max-w-5xl mx-auto relative">
+      <div className="absolute top-4 right-4 text-sm text-gray-500 italic">
+        By: Tera Bhai
+      </div>
+      <h1 className="text-3xl font-bold mb-6 text-center">📰 Sarkari Job Alerts</h1>
+      {/* Cards */}
+      ...
+    </div>
+  );
+}
+export default App;
